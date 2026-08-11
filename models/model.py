4513994494b6e25147de5b35b0995ea7a3fbcfd0
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-08-11 16:20:33 -0400

    Started the model

---
+++ models/model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from scipy.stats import entropy
+import seaborn as sns
+import matplotlib.pyplot as plt
+
